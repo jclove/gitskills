e3d403079a2379ec079d28a5e9b1ce9d28ddd48c
chiwengang <chiwg@weiyingjia.cn>
2017-08-14 14:16:30 +0800

    修改

--- 1.php
+++ 1.php
@@ -1,1 +1,17 @@
-kakjksak
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>点餐器</title>
+	<link rel="stylesheet" href="./baidu.css"/>
+	<style>
+		*{padding:0;margin:0;}
+ 		body{text-align:center}
+	</style>
+</head>
+<body>
+	<div>
+		<input type='text' name='name' id='name'>
+	</div>
+</body>
+</html>
